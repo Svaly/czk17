dcdbefed00d72e95f07e69f0eea77b3e9333563e
Rafał <rafal.grzesik94@gmail.com>
2020-02-01 19:06:15 +0100

    czk17 app

---
+++ src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
+// Bootstrap module
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+// Feature components
+import { ExchangeRatesComponent } from './exchange-rates/exchange-rates.component';
+import { ExchangeRatesFilterComponent } from './exchange-rates/exchange-rates-filter/exchange-rates-filter.component';
+import { ExchangeRatesTableComponent } from './exchange-rates/exchange-rates-table/exchange-rates-table.component';
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    ExchangeRatesComponent,
+    ExchangeRatesFilterComponent,
+    ExchangeRatesTableComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule,
+    FormsModule,
+    HttpClientModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
